1de066407727bbefab49ee50ed474d01e9a52159
Yianlaen <blahduckling747@qq.com>
2024-12-13 16:44:53 +0800

    chore: change styles

--- src/App.java
+++ src/App.java
@@ -1,4 +1,6 @@
 import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 import model.Accounts;
 import view.GameWindow;
@@ -6,8 +8,14 @@ import view.GameWindow;
 public class App {
     public static void main(String[] args) throws Exception {
         java.util.Locale.setDefault(java.util.Locale.ENGLISH);
-        System.out.println("Sokoban project started!");
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException
+                | UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
         Accounts.load4file();
+        System.out.println("Sokoban project started!");
         SwingUtilities.invokeLater(() -> {
             new GameWindow();
         });
